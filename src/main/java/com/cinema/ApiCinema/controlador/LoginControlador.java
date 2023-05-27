af6a18fe6d6b9443bbef593c40f18d60f3e92ace
Arthur Silva Rodrigues <asr.1998x@gmail.com>
2023-05-27 12:54:14 -0300

    login

--- src/main/java/com/cinema/ApiCinema/controlador/LoginControlador.java
+++ src/main/java/com/cinema/ApiCinema/controlador/LoginControlador.java
@@ -1,7 +1,12 @@
 package com.cinema.ApiCinema.controlador;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +19,25 @@ public class LoginControlador {
 	@Autowired
 	private UsuarioRepositorio usuarioRepositorio;
 	
-	@GetMapping(value = "/login")
-	public @ResponseBody Iterable<Usuario> getUsuario() { 
-		return usuarioRepositorio.findAll();
-	}
-
+    @CrossOrigin
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestParam Map<String, String> usuario ){
+    	Usuario _usuario = new Usuario();
+   
+    	_usuario.setSenha(usuario.get("senha"));
+      	 usuarioRepositorio.findByEmail(usuario.get("email"));
+    	return usuario;
+    }
+	
+    @CrossOrigin
+    @PostMapping("/cadastro")
+    public Map<String, String> cadastro(@RequestParam Map<String, String> usuario ){
+    	Usuario _usuario = new Usuario();
+    	_usuario.setEmail(usuario.get("email"));
+    	_usuario.setSenha(usuario.get("senha"));
+      	 usuarioRepositorio.save(_usuario);
+    	return usuario;
+    }
+	
 }
 			
--- src/main/java/com/cinema/ApiCinema/repository/UsuarioRepositorio.java
+++ src/main/java/com/cinema/ApiCinema/repository/UsuarioRepositorio.java
@@ -1,11 +1,13 @@
 package com.cinema.ApiCinema.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.cinema.ApiCinema.model.Usuario;
 
 public interface UsuarioRepositorio extends CrudRepository<Usuario, Integer> {
-	
+		List<Usuario> findByEmail(String email);
 	
 
 }
